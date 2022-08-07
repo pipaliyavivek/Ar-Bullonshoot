a32424c9199f1189608b9db45c375d3cc0a1953e
Pipaliyavivek <Pipaliyavivek308@gmail.com>
2022-08-07 12:18:34 +0530

    SMALL CHANGE

---
+++ AR DEMO_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,101 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void Balloon::Start()
+extern void Balloon_Start_mCD4C88738C46E1E4F43655FE2391C27F781872ED (void);
+// 0x00000002 System.Void Balloon::Update()
+extern void Balloon_Update_m331242438CFD98B46D5E2E9831C43DBC282F3A1E (void);
+// 0x00000003 System.Void Balloon::.ctor()
+extern void Balloon__ctor_m3BA592BAA0D0CC161DAD0514F588E3BD644DAA73 (void);
+// 0x00000004 System.Void shoot::Update()
+extern void shoot_Update_mADBB866D948AE5FB79294F50849334E9101FA8CE (void);
+// 0x00000005 System.Void shoot::Shoot()
+extern void shoot_Shoot_mE691D1449996A8DEB8A819B3A4EE002A5CCF600F (void);
+// 0x00000006 System.Void shoot::.ctor()
+extern void shoot__ctor_mCA8EEF9D9F45D955700A0FC466406D10DD170128 (void);
+// 0x00000007 System.Void Spawnballon::Start()
+extern void Spawnballon_Start_m85FB6801298C317A466BA25100146AF0A0D3553A (void);
+// 0x00000008 System.Collections.IEnumerator Spawnballon::StartSpawning()
+extern void Spawnballon_StartSpawning_m09FFD84FC1C8554AA96AD417B5BF630C6CB201B6 (void);
+// 0x00000009 System.Void Spawnballon::.ctor()
+extern void Spawnballon__ctor_m97FC5B5ABEF477BA52970E005DBBFAF7601C1329 (void);
+// 0x0000000A System.Void Spawnballon/<StartSpawning>d__3::.ctor(System.Int32)
+extern void U3CStartSpawningU3Ed__3__ctor_m907B0A67F7495A85D228088A461D8D3997E43C44 (void);
+// 0x0000000B System.Void Spawnballon/<StartSpawning>d__3::System.IDisposable.Dispose()
+extern void U3CStartSpawningU3Ed__3_System_IDisposable_Dispose_m627A55B71A581651E0962CF680A4A9E509F22741 (void);
+// 0x0000000C System.Boolean Spawnballon/<StartSpawning>d__3::MoveNext()
+extern void U3CStartSpawningU3Ed__3_MoveNext_m9F9DE78D3DA48A6DF477DF41B6D0ED1AE0C9E776 (void);
+// 0x0000000D System.Object Spawnballon/<StartSpawning>d__3::System.Collections.Generic.IEnumerator<System.Object>.get_Current()
+extern void U3CStartSpawningU3Ed__3_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_m1EE3FFC2E5F7149B752A6EEDF7954D9D30AB6FC0 (void);
+// 0x0000000E System.Void Spawnballon/<StartSpawning>d__3::System.Collections.IEnumerator.Reset()
+extern void U3CStartSpawningU3Ed__3_System_Collections_IEnumerator_Reset_mE08763166C3F3E47B714BDA72629E6D4AD0FBCBD (void);
+// 0x0000000F System.Object Spawnballon/<StartSpawning>d__3::System.Collections.IEnumerator.get_Current()
+extern void U3CStartSpawningU3Ed__3_System_Collections_IEnumerator_get_Current_m6162AF7126912CD06382E96F4EFAACCC27138087 (void);
+static Il2CppMethodPointer s_methodPointers[15] = 
+{
+	Balloon_Start_mCD4C88738C46E1E4F43655FE2391C27F781872ED,
+	Balloon_Update_m331242438CFD98B46D5E2E9831C43DBC282F3A1E,
+	Balloon__ctor_m3BA592BAA0D0CC161DAD0514F588E3BD644DAA73,
+	shoot_Update_mADBB866D948AE5FB79294F50849334E9101FA8CE,
+	shoot_Shoot_mE691D1449996A8DEB8A819B3A4EE002A5CCF600F,
+	shoot__ctor_mCA8EEF9D9F45D955700A0FC466406D10DD170128,
+	Spawnballon_Start_m85FB6801298C317A466BA25100146AF0A0D3553A,
+	Spawnballon_StartSpawning_m09FFD84FC1C8554AA96AD417B5BF630C6CB201B6,
+	Spawnballon__ctor_m97FC5B5ABEF477BA52970E005DBBFAF7601C1329,
+	U3CStartSpawningU3Ed__3__ctor_m907B0A67F7495A85D228088A461D8D3997E43C44,
+	U3CStartSpawningU3Ed__3_System_IDisposable_Dispose_m627A55B71A581651E0962CF680A4A9E509F22741,
+	U3CStartSpawningU3Ed__3_MoveNext_m9F9DE78D3DA48A6DF477DF41B6D0ED1AE0C9E776,
+	U3CStartSpawningU3Ed__3_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_m1EE3FFC2E5F7149B752A6EEDF7954D9D30AB6FC0,
+	U3CStartSpawningU3Ed__3_System_Collections_IEnumerator_Reset_mE08763166C3F3E47B714BDA72629E6D4AD0FBCBD,
+	U3CStartSpawningU3Ed__3_System_Collections_IEnumerator_get_Current_m6162AF7126912CD06382E96F4EFAACCC27138087,
+};
+static const int32_t s_InvokerIndices[15] = 
+{
+	2074,
+	2074,
+	2074,
+	2074,
+	2074,
+	2074,
+	2074,
+	2042,
+	2074,
+	1706,
+	2074,
+	2010,
+	2042,
+	2074,
+	2042,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	15,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
